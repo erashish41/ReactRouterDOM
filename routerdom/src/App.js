cd9ceeead87e5c9ecd46be14128b2cf38069f6cb
Ashish Bhardwaj <erasish41@gmail.com>
2023-12-05 16:41:13 +0530

    path added

--- routerdom/src/App.js
+++ routerdom/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Router
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<div>Home Page</div>} />
+          <Route path = "/about" element = {<div>About Page</div>}/>
+          <Route path = "/contact" element = {<div>Contact Page</div>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
